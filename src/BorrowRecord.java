05506a3b4a9a44ab98f1bfb08047e6f3f808738b
harsh <harshadeshappriya999@gmail.com>
2025-12-23 13:12:27 +0530

    Add BorrowRecord class and methods to track borrowing and returning of books

--- src/BorrowRecord.java
+++ src/BorrowRecord.java
@@ -1,2 +1,26 @@
+import java.util.Date;
+
 public class BorrowRecord {
+    private int id;
+    private int userId;
+    private int bookId;
+    private Date borrowDate;
+    private Date returnDate;
+
+    public BorrowRecord(int id, int userId, int bookId, Date borrowDate, Date returnDate) {
+        this.id = id;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+    }
+
+    public int getId() { return id; }
+    public int getUserId() { return userId; }
+    public int getBookId() { return bookId; }
+    public Date getBorrowDate() { return borrowDate; }
+    public Date getReturnDate() { return returnDate; }
+
+    public void setBorrowDate(Date borrowDate) { this.borrowDate = borrowDate; }
+    public void setReturnDate(Date returnDate) { this.returnDate = returnDate; }
 }
