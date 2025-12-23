a28a81ecb13d622ecf6cdb269b61beeda66defe6
harsh <harshadeshappriya999@gmail.com>
2025-12-23 13:08:19 +0530

    Add Book class and CRUD methods for books in LibraryService

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,22 @@
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(int id, String title, String author, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean getIsAvailable() { return isAvailable; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+    public void setIsAvailable(boolean isAvailable) { this.isAvailable = isAvailable; }
 }
